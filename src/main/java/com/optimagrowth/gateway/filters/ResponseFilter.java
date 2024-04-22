53afe0011fe3cb07e12aef1aff14330db44496bd
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-04-22 15:39:25 -0400

    Add response filter

---
+++ src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
@@ -0,0 +1,37 @@
+package com.optimagrowth.gateway.filters;
+
+import static com.optimagrowth.gateway.util.FilterUtil.CORRELATION_ID;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.optimagrowth.gateway.util.FilterUtil;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+class ResponseFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        HttpHeaders httpHeaders = request.getHeaders();
+        String correlationId = FilterUtil.getCorrelationId(httpHeaders);
+
+        log.debug("Adding the correlation ID to the outbound headers: {}", correlationId);
+
+        exchange.getResponse().getHeaders().add(CORRELATION_ID, correlationId);
+
+        log.debug("Completing outgoing request for {}.", request.getURI());
+
+        return chain.filter(exchange);
+    }
+
+}
