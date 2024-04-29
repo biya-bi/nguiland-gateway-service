8ab8f759102d7d19a3210dc9d4f1df57025453ee
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-04-28 23:14:34 -0400

    Configure CORS

--- src/main/java/com/optimagrowth/gateway/config/SecurityConfig.java
+++ src/main/java/com/optimagrowth/gateway/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.optimagrowth.gateway.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,6 +10,9 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,10 +21,26 @@ class SecurityConfig {
     @Bean
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity,
             ServerAuthenticationEntryPoint entryPoint) {
-        return httpSecurity.authorizeExchange(customizer -> customizer.anyExchange().authenticated())
-                .oauth2ResourceServer(customizer -> customizer.authenticationEntryPoint(entryPoint)
-                        .jwt(Customizer.withDefaults()))
-                .exceptionHandling(customizer -> customizer.authenticationEntryPoint(entryPoint)).build();
+        return httpSecurity.authorizeExchange(c -> c.anyExchange().authenticated())
+                .oauth2ResourceServer(c -> c.authenticationEntryPoint(entryPoint).jwt(Customizer.withDefaults()))
+                .cors(Customizer.withDefaults())
+                .exceptionHandling(c -> c.authenticationEntryPoint(entryPoint))
+                .build();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(
+            @Value("${security.cors.allowedOrigins:}") String[] allowedOrigins,
+            @Value("${security.cors.allowedMethods:}") String[] allowedMethods,
+            @Value("${security.cors.allowedHeaders:}") String[] allowedHeaders) {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        configuration.setAllowedMethods(Arrays.asList(allowedMethods));
+        configuration.setAllowedHeaders(Arrays.asList(allowedHeaders));
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
